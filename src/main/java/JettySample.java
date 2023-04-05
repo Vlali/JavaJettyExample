a91b5e14b0048bd9189a7faafc864888aa28cb8c
Ľudovít Vendrics <vendrics.ludovit@gmail.com>
2023-04-05 23:12:26 +0200

    Initial commit

---
+++ src/main/java/JettySample.java
@@ -0,0 +1,15 @@
+
+import org.mortbay.jetty.Server;
+import org.mortbay.jetty.handler.ResourceHandler;
+import org.mortbay.jetty.servlet.Context;
+public class JettySample {
+    public static void main(String[] args) throws Exception {
+        Server server = new Server(8081);
+
+        Context root = new Context(server, "/");
+        root.setResourceBase(".");
+        root.setHandler(new ResourceHandler());
+
+        server.start();
+    }
+}
